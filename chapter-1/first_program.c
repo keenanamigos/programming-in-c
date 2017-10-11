2245ee726a9a07d6cd1c2a3d183f58a579093e5a
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-10 20:47:35 -0500

    Chapter 1 Examples progress: Program 3.4

---
+++ chapter-1/first_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// stdio.h is a header file and should be included in just about every C program
+int main(void)
+{
+    printf("This is my first C program (but not really)!\n");
+
+    return 0; // Convention to indicate that the program has complete successfully
+}
---
+++ chapter-1/program-3-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("C is a fun language!\n");
+    printf("Can't wait to continue learning.\n");
+
+    return 0;
+}
