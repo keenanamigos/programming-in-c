24e61d4f50deda912133314398690b29cc489d70
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-11 10:23:19 -0500

    Finished Chapter 1 Exercise 5

---
+++ chapter-1/exercises/exercise_5.c
@@ -0,0 +1,27 @@
+// Fix the errors in the original program
+// *** Original Program *** //
+/* 
+#include <stdio.h>
+
+int main (Void)
+(
+    INT sum;
+    // COMPUTE RESULT
+    sum = 25 + 37 - 19
+    // DISPLAY RESULTS //
+    printf ("The answer is %i\n" sum);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int sum;
+    sum = 25 + 37 - 19;
+    
+    printf("The answer is %i\n", sum);
+
+    return 0;
+}
