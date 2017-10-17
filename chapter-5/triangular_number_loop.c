58d7f1fd26d4686b17bcae748aef106c097710ec
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-17 12:29:25 -0500

    Program 5.2: 200th Triangular Number

---
+++ chapter-5/triangular_number_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int triangularNumber = 0;
+
+    for (n = 1; n <= 200; n = n + 1)
+    {
+        triangularNumber = triangularNumber + n;
+    }
+
+    printf("The 200th triangular number is %i\n", triangularNumber);
+
+    return 0;
+}
