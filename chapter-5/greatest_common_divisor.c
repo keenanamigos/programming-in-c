a135b827b9618f85c1f0b5958bf2835eec3a1865
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-18 10:57:59 -0400

    Program 5.7: Greatest Common Divisor

---
+++ chapter-5/greatest_common_divisor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int u, v, temp;
+
+    printf("Please type in two non-negative integers.\n");
+    scanf("%i%i", &u, &v);
+
+    while (v != 0)
+    {
+        temp = u % v;
+        u = v;
+        v = temp;
+    }
+
+    printf("Their greatest common divisor is %i\n", u);
+
+    return 0;
+}
