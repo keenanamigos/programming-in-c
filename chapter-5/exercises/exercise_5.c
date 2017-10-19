1982b84a0012a9d94f05713dcf8a84926a560de2
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-19 17:33:50 -0400

    Make n column left-justified

--- chapter-5/exercises/exercise_5.c
+++ chapter-5/exercises/exercise_5.c
@@ -20,14 +20,14 @@ int main(void)
     int n, two_to_the_n;
 
     printf("TABLE OF POWERS OF TWO\n\n");
-    printf(" n 2 to the n\n");
+    printf("n   2 to the n\n");
     printf("--- ---------\n");
 
     two_to_the_n = 1;
 
     for(n = 0; n <= 10; ++n)
     {
-        printf("%2i %6i\n", n, two_to_the_n); 
+        printf("%-2i %6i\n", n, two_to_the_n); 
         two_to_the_n*=2;
     }
 
