3fd4ed7e4ed37cb07a86191c34c997ca413e3d92
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-19 17:32:45 -0400

    Finish exercise 5

---
+++ chapter-5/exercises/exercise_5.c
@@ -0,0 +1,35 @@
+/* 
+    Re-write this program to make it more readable
+
+    #include <stdio.h>
+    int main(void){
+    int n,two_to_the_n;
+    printf("TABLE OF POWERS OF TWO\n\n");
+    printf(" n 2 to the n\n");
+    printf("--- ---------------\n");
+    two_to_the_n=1;
+    for(n=0;n<=10;++n){
+    printf("%2i %i\n",n,two_to_the_n); two_to_the_n*=2;}
+    return 0;}
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, two_to_the_n;
+
+    printf("TABLE OF POWERS OF TWO\n\n");
+    printf(" n 2 to the n\n");
+    printf("--- ---------\n");
+
+    two_to_the_n = 1;
+
+    for(n = 0; n <= 10; ++n)
+    {
+        printf("%2i %6i\n", n, two_to_the_n); 
+        two_to_the_n*=2;
+    }
+
+    return 0;
+}
