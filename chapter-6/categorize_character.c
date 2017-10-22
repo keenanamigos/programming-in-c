4313962e9c2223a9bf5eeccb8a190a98c9ee85c8
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-22 15:19:11 -0400

    Program 6.7: Categorize Characters

---
+++ chapter-6/categorize_character.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+
+    printf("Please enter a single character: ");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        printf("It's an alphabetic character.\n");
+    }
+    else if (c >= '0' && c <= '9')
+    {
+        printf("It's a digit.\n");
+    }
+    else
+    {
+        printf("It's a special character.\n");
+    }
+
+    return 0;
+}
