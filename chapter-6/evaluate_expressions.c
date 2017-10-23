10ab4d24b0eb718b3077bcfe5afd226d742f1ed2
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-23 16:17:09 -0400

    Program 6.8 (using switch/case): Evaluate Expressions

--- chapter-6/evaluate_expressions.c
+++ chapter-6/evaluate_expressions.c
@@ -8,25 +8,26 @@ int main(void)
     printf("Please type your expression: ");
     scanf("%f %c %f", &value1, &operator, &value2);
     
-    if(operator == '+')
+    switch (operator)
     {
-        printf("%.2f\n", value1 + value2);
-    }
-    else if (operator == '-')
-    {
-        printf("%.2f\n", value1 - value2);
-    }
-    else if (operator == '*')
-    {
-        printf("%.2f\n", value1 * value2);
-    }
-    else if (operator == '/')
-    {
-        printf("%.2f\n", value1 / value2);
-    }
-    else
-    {
-        printf("Please enter a symbol for addition, subtraction, multiplication, or division.\n");
+        case '+':
+            printf("%.2f\n", value1 + value2);
+            break;
+
+        case '-':
+            printf("%.2f\n", value1 - value2);
+            break;
+
+        case '*':
+            printf("%.2f\n", value1 * value2);
+            break;
+
+        case '/':
+            printf("%.2f\n", value1 / value2);
+            break;
+
+        default:
+            printf("Please enter a symbol for addition, subtraction, multiplication, or division.\n");
     }
 
     return 0;
